55c60605df9f477cb6143849d8db8423c7fc8378
Ivan Khomich <44943882+grippvh@users.noreply.github.com>
2023-02-09 00:57:04 +0100

    added showing 6 pokemons pro page

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { lastValueFrom } from 'rxjs';
+import {lastValueFrom, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,21 +13,21 @@ export class ApiService {
    * @param offset how many records to skip
    * @param limit how many records to return
    * @returns list of pokemons
-   * 
+   *
    * @url https://pokeapi.co/docs/v2#resource-listspagination-section
    */
-  get(offset = 20, limit = 20) {
-    return lastValueFrom(this.http.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`));
+  get(offset = 0, limit = 6): Observable<any> {
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
   }
 
   /**
    * Fetch a single pokemon from API
    * @param id id or name of the pokemon
    * @returns pokemon
-   * 
+   *
    * @url https://pokeapi.co/docs/v2#pokemon
    */
-  getOne(id: number | string) {
-    return lastValueFrom(this.http.get(`https://pokeapi.co/api/v2/pokemon/${id}`));
+  getOne(id: number | string): Observable<any>{
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from "./api.service";
+
+class Pokemon {
+  name: string = "";
+  url: string = "";
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'pokedex';
+  pokemons: Pokemon[] = []
+  currentPage = 0;
+  limit = 6;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+      this.getPokemons()
+  }
+
+  getPokemons() {
+    this.apiService.get(this.currentPage * this.limit, this.limit).subscribe(result => {
+      this.pokemons = result.results;
+    });
+  }
+
+  nextPage() {
+    this.currentPage++;
+    this.getPokemons();
+  }
+
+  previousPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.getPokemons();
+    }
+  }
+
 }
